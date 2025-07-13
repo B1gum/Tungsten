238be9882a257fa237ffb8e50ee35287ec557b50
Noah Rahbek Bigum Hansen <noah.r.b.hansen3.14@gmail.com>
2025-07-13 17:19:25 +0200

    Test: Add unit test for parse_wolfram_error

---
+++ tests/unit/util/wolfram_error_spec.lua
@@ -0,0 +1,23 @@
+-- tests/unit/util/wolfram_error_spec.lua
+-- Unit tests for tungsten.util.wolfram_error.parse_wolfram_error
+
+local error_parser = require 'tungsten.util.wolfram_error'
+
+describe("util.wolfram_error.parse_wolfram_error", function()
+  it("recognizes Name::tag style errors", function()
+    local err = error_parser.parse_wolfram_error("Solve::nsmet: no solution")
+    assert.are.equal("Solve::nsmet: no solution", err)
+  end)
+
+  it("parses Message[Name::tag, \"...\"] format", function()
+    local err = error_parser.parse_wolfram_error('Message[Solve::nsmet, "no solution"]')
+    assert.are.equal("Solve::nsmet: no solution", err)
+  end)
+
+  it("returns nil for unrecognized output", function()
+    local err = error_parser.parse_wolfram_error("Some unrelated output")
+    assert.is_nil(err)
+  end)
+end)
+
+
