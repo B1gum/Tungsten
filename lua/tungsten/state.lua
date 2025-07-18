b4de317efcb46f94886b69dc9c82bb5e365d2013
Noah Rahbek Bigum Hansen <noah.r.b.hansen3.14@gmail.com>
2025-07-18 23:32:49 +0200

    Chore: Expose configuration in state

--- lua/tungsten/state.lua
+++ lua/tungsten/state.lua
@@ -9,7 +9,7 @@ local M = {}
 M.ns = vim.api.nvim_create_namespace("tungsten")
 M.active_jobs = {}
 M.cache = Cache.new(config.cache_max_entries, config.cache_ttl)
-M.config = {}
+M.config = config
 M.persistent_variables = {}
 
 return M
