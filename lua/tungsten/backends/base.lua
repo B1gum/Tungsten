24fc263448608ebb87772f4eeb3597ef311c5d60
Noah Rahbek Bigum Hansen <noah.r.b.hansen3.14@gmail.com>
2025-07-31 17:47:44 +0200

    Feat: Define stub abstract backend interface

---
+++ lua/tungsten/backends/base.lua
@@ -0,0 +1,23 @@
+local BaseBackend = {}
+
+function BaseBackend.ast_to_code(ast)
+    error("Not implemented")
+end
+
+function BaseBackend.ast_to_string(ast)
+    error("Not implemented")
+end
+
+function BaseBackend.evaluate_async(ast, numeric, callback)
+    error("Not implemented")
+end
+
+function BaseBackend.solve_async(eq_asts, var_asts, is_system, callback)
+    error("Not implemented")
+end
+
+function BaseBackend.load_handlers()
+    error("Not implemented")
+end
+
+return BaseBackend
